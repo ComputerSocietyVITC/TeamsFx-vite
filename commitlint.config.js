29188fdca561fd115e25a395a6ee22314624980a
AbhijithGanesh <git.watch.Abhijith@outlook.com>
2023-03-15 23:26:40 +0530

    Addition of new vite support
    Signed-off-by: AbhijithGanesh <git.watch.Abhijith@outlook.com>

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    extends: ["@commitlint/config-conventional"],
-    "type-enum": [
-        2,
-        "always",
-        ["chore", "docs", "feat", "fix", "refactor", "style", "test"],
-    ],
-    "scope-empty": [2, "never"],
+  extends: ["@commitlint/config-conventional"],
+  "type-enum": [
+    2,
+    "always",
+    ["chore", "docs", "feat", "fix", "refactor", "style", "test"],
+  ],
+  "scope-empty": [2, "never"],
 };
